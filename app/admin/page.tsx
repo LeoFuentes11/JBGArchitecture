037661640638e59f9102819f18f3399b125071c4
Leo Fuentes <leo.emile.fuentes@gmail.com>
2026-04-14 22:55:57 +0930

    fix: remove /admin page (needs root layout)

--- app/admin/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function Admin() {
-  const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'sv48v185'
-  redirect(`https://www.sanity.io/desk;${projectId}`)
-}
